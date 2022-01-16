508ac07fb35f005e25b262f03308540dfb943b02
Antonio <antoniofinisterrafreitas@gmail.com>
2022-01-16 11:57:42 +0000

    16/01 - 11:57

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const mongo = require('mongodb');
+const assert = require('assert')
+
+const url = 'mongodb://localhost:5000';
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -8,5 +12,43 @@ router.get('/', (req, res) => {
 router.get('/carro1', (req, res) => {
     res.render('carro1')
   })
+
+router.get('/admin', (req, res) => {
+  res.render('admin')
+})
+
+
+router.get('/receber-dados', function(req, res, next){
+  var Lista = [];
+  mongo.connect(url, function(err,db){
+    assert.equal(null, err);
+    var cursor = db.collection('dados utilizador').find();
+    cursor.forEach(function(doc, err){
+      assert.equal(null, err);
+      Lista.push(doc);
+    }, function(){
+      db.close;
+      res.render('index', {items: resultArray});
+    })
+  })
+
+})
+
+router.post('/publicar', function(req,res,next){
+  var item = {
+    marca: req.body.marca,
+    modelo: req.body.modelo,
+    ano: req.body.ano,
+  };
+
+  mongo.ConnectionCheckOutFailedEvent(url, function(err,db){
+    assert.equal(null,err);
+    db.collection('user-data'.insertOne(item, function(err, result){
+      assert.equal(null, error);
+      console.log('publicado');
+      db.close();
+    }))
+  })
+})
   
 module.exports = router
